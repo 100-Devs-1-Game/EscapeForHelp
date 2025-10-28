fca26c70d8d688e551ac50e7c8caea3fe0158ae4
Scrawach <scrawach@gmail.com>
2025-10-28 16:52:11 +0300

    refactor: make some export fields for experience gem, use inspector material instead of create new one

--- src/scenes/objects/experience_gem.gd
+++ src/scenes/objects/experience_gem.gd
@@ -2,18 +2,19 @@ class_name ExperienceGem
 extends Area3D
 
 @export var experience : int = 1
-
-var target : Node3D = null
-var speed : float = -1
+@export var speed: float = -0.3
+@export var pickup_speed_step: float = 4
 
 @onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
 @onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
 @onready var snd_collected: AudioStreamPlayer = $SndCollected
 
+var target : Node3D = null
+
 func _ready() -> void:
 	# Create a new material to override the mesh color based on experience value
-	var material: StandardMaterial3D = StandardMaterial3D.new()
-
+	var material: StandardMaterial3D = mesh_instance_3d.get_surface_override_material(0)
+	
 	if experience < 5:
 		material.albedo_color = Color(0.2, 0.5, 1.0)
 	elif experience < 25:
@@ -23,12 +24,10 @@ func _ready() -> void:
 	else:
 		material.albedo_color = Color(0.8, 0.2, 1.0)
 
-	mesh_instance_3d.material_override = material
-
 func _physics_process(delta : float) -> void:
 	if target != null:
 		global_position = global_position.move_toward(target.global_position, speed)
-		speed += 5 * delta
+		speed += pickup_speed_step * delta
 		
 func collect() -> int:
 	snd_collected.play()
